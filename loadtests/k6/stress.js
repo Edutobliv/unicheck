b8db3d53982d26b8b64ccbd3f28cc60c9f9e49cd
Edutobliv <edut6009@gmail.com>
2025-10-19 22:05:10 -0500

    Update stress.js

--- loadtests/k6/stress.js
+++ loadtests/k6/stress.js
@@ -3,10 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 20 },
-    { duration: '3m', target: 100 },
-    { duration: '1m', target: 200 },
-    { duration: '3m', target: 100 },
+    { duration: '2m', target: 20 },
+    { duration: '3m', target: 50 },
+    { duration: '2m', target: 50 },
     { duration: '1m', target: 0 },
   ],
   thresholds: {
@@ -40,4 +39,3 @@ export default function () {
 
   sleep(0.5);
 }
-
